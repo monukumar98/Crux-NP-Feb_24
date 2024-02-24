671cb43511a8fb971a200a7b8ff3d0630dde56ce
monukumar98 <monu012as@gmail.com>
2024-02-24 15:19:01 +0530

    Pattern-1

---
+++ Lec2/Pattern22.java
@@ -0,0 +1,43 @@
+package Lec2;
+
+import java.util.Scanner;
+
+public class Pattern22 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int row = 1;
+		int star = n;
+		int space =-1;
+		while (row <= n) {
+			// star
+			int i = 1;
+			while (i <= star) {
+				System.out.print("* ");
+				i++;
+			}
+			// space
+			int j = 1;
+			while (j <= space) {
+				System.out.print("  ");
+				j++;
+			}
+			// star
+			int k = 1;
+			if(row==1) {
+				k=2;
+			}
+			while (k <= star) {
+				System.out.print("* ");
+				k++;
+			}
+			// next row Prep
+			row++;
+			star--;
+			space += 2;
+			System.out.println();
+
+		}
+	}
+}
