4c3bca802975856850775ff4bd758d5c7f9e56d6
monukumar98 <monu012as@gmail.com>
2024-03-17 15:25:15 +0530

    Array Qps

--- Lec8/Reversal_Algo.java
+++ Lec8/Reversal_Algo.java
@@ -7,11 +7,32 @@ public class Reversal_Algo {
 		int[] arr = { 10, 20, 30, 40, 50, 60, 70 };
 		int k = 3;
 		Rotate(arr, k);
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
 	}
 
 	public static void Rotate(int[] arr, int k) {
 		int n = arr.length;
 		k = k % n;
+		// starting ke n-k element
+		Reverse(arr, 0, n - k - 1);
+		// last ke k element
+		Reverse(arr, n - k, n - 1);
+		// reverse all elemet
+		Reverse(arr, 0, n - 1);
+	}
+
+	public static void Reverse(int[] arr, int i, int j) {
+		while (i < j) {
+			int temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+			i++;
+			j--;
+
+		}
+
 	}
 
 }
