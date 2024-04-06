81a43e623178fcd47dfd26e4344b9f47abd6fcd1
monukumar98 <monu012as@gmail.com>
2024-04-06 16:44:54 +0530

    Time and Space

--- Lec11/Time_and_Space_comp.java
+++ Lec11/Time_and_Space_comp.java
@@ -34,9 +34,79 @@ public class Time_and_Space_comp {
 			// O(log(N))
 			n /= 2;
 		}
+
+		while (i <= n) {
+			System.out.println("Hey");
+			// O(N)
+			i += 2;
+			i += 3;
+		}
+		while (i <= n) {
+			System.out.println("Hey");
+			// O(log(N)) base 6
+			i *= 2;
+			i *= 3;
+		}
+		while (n > 0) {
+			System.out.println("Hey");
+			// O(log(N)) base 6
+			n /= 2;
+			n /= 3;
+		}
+		int k = 2;
+		while (i <= n) {
+			System.out.println("Hey");
+			// O(N/k)
+			i += k;
+		}
+		while (i <= n) {
+			System.out.println("Hey");
+			// O(log(N)) base k
+			i *= k;
+		}
+		for (i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				// O(N^2)
+				System.out.println("hey");
+			}
+		}
+		for (i = 1; i * i <= n; i++) {
+			// O(Sqrt(N))
+			System.out.println("hey");
+		}
+		for (i = 1; i <= n; i++) {
+			for (int j = 1; j <= i * i; j++) {
+				for (k = 1; k <= n / 2; k++) {
+					System.out.println("hey");
+				}
+			}
+		}
+		for (i = 1; i <= n; i *= 2) {
+			// O(log(N) base 2
+			System.out.println("hey");
+		}
+
+		for (i = n / 2; i <= n; i++) {
+			for (int j = 1; j <= n / 2; j++) {
+				for (k = 1; k <= n; k = k * 2) {
+					// O(N^2 log(N))
+					System.out.println("hey");
+				}
+			}
+		}
+		for (i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j += i) {
+				System.out.println("hey");
+			}
+		}
+
 	}
 
 }
+
+
+
+
 
 
 
