cc22516e87e8def5941bb6e76333c5c796e1643a
monukumar98 <monu012as@gmail.com>
2024-04-21 15:23:47 +0530

    Recursion-2

---
+++ Lec16/Coin_Toss.java
@@ -0,0 +1,20 @@
+package Lec16;
+
+public class Coin_Toss {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=3;
+		Toss(n, "");
+
+	}
+	public static void Toss(int n,String ans) {
+		if(n==0) {
+			System.out.println(ans);
+			return;
+		}
+		Toss(n-1, ans+"H");
+		Toss(n-1, ans+"T");
+	}
+
+}
---
+++ Lec16/Coin_Toss_2.java
@@ -0,0 +1,22 @@
+package Lec16;
+
+public class Coin_Toss_2 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 3;
+		Toss(n, "");
+
+	}
+
+	public static void Toss(int n, String ans) {
+		if (n == 0) {
+			System.out.println(ans);
+			return;
+		}
+		if (ans.length() == 0 || ans.charAt(ans.length() - 1)!='H') {
+			Toss(n - 1, ans + "H");
+		}
+		Toss(n - 1, ans + "T");
+	}
+
+}
