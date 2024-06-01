ab7d0af53ea3cd13ab04ff3112aea44016569133
monukumar98 <monu012as@gmail.com>
2024-06-01 15:18:12 +0530

    Stack_qps

---
+++ Lec26/Insert_In_last.java
@@ -0,0 +1,30 @@
+package Lec26;
+
+import java.util.Stack;
+
+public class Insert_In_last {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Stack<Integer> st = new Stack<>();
+		st.push(10);
+		st.push(20);
+		st.push(30);
+		st.push(40);
+		st.push(50);
+		System.out.println(st);
+		Add(st, 60);
+		System.out.println(st);
+
+	}
+
+	public static void Add(Stack<Integer> st, int item) {
+		if (st.isEmpty()) {
+			st.push(item);
+			return;
+		}
+		int x = st.pop();
+		Add(st, item);
+		st.push(x);
+	}
+}
